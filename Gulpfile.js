4f43b0e354aa53a1753a2b99addf926e31f7eb08
Quintin Henn <quintin.henn@gmail.com>
2019-06-07 07:55:10 +0200

    Build, serve and watch sass, templates and js files

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,4 +1,5 @@
-const { src, dest, series } = require("gulp");
+const { src, dest, watch, series, parallel } = require("gulp");
+const data = require('gulp-data');
 const rimraf = require("gulp-rimraf");
 const nunjucksRender = require("gulp-nunjucks-render");
 const autoprefixer = require("gulp-autoprefixer");
@@ -12,11 +13,14 @@ const sass = require("gulp-sass");
 sass.compiler = require("node-sass");
 
 const siteOutput = "dist/";
-const inputTemplates = "app/pages/**/*.+(html|njk)";
-
-const input = "app/scss/*.scss";
+const inputPages = "app/pages/**/*.+(html|njk)";
+const inputTemplates = "app/templates/**/*.+(html|njk)";
+const inputSass = "app/scss/*.scss";
 const inputMain = "app/scss/main.scss";
-const output = siteOutput + "/css";
+const inputStatic = "app/static/*";
+const inputCSS = "app/css/*.css";
+const inputScripts = "app/scripts/*.js";
+const outputStyles = siteOutput + "/css";
 
 var sassOptions = {
   outputStyle: "expanded"
@@ -28,46 +32,78 @@ var sassdocOptions = {
   dest: siteOutput + "/sassdoc"
 };
 
-function cleanTask () {
+function cleanTask() {
   return src(siteOutput, {
-    read: false
+    read: false,
+    allowEmpty: true
   }).pipe(rimraf());
 }
 
-function sassTask () {
-  return src(inputMain).pipe(sass(sassOptions).on("error", sass.logError)).pipe(
-    autoprefixer(autoprefixerOptions)
-  ).pipe(dest(output)).pipe(browserSync.stream());
+function sassTask() {
+  return src(inputMain)
+    .pipe(sass(sassOptions).on("error", sass.logError))
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(dest(outputStyles))
+    .pipe(browserSync.stream());
 }
 
-function scriptTask () {
+function scriptTask() {
   return src(
-    ["./bower_components/bootstrap-sass/assets/javascripts/bootstrap.js",
-      "js/main.js"]
+    ["app/scripts/plugins.js", "app/scripts/main.js"]
   ).pipe(concat({
     path: "main.js"
-  })).pipe(browserSync.reload({
+  }))
+  .pipe(browserSync.reload({
     stream: true
-  })).pipe(
-    gulp.dest(
-      siteOutput + "/js"
-    )
-  );
+  }))
+  .pipe(dest(siteOutput + "/js"));
 }
 
-function nunjucksTask () {
-  return src(inputTemplates).pipe(nunjucksRender({
-    path: ["app/templates"]
-  })).pipe(
-    dest(siteOutput)
-  );
+function nunjucksTask() {
+  return src(inputPages)
+    .pipe(data(function () {
+      return require('./app/data.json')
+    }))
+    .pipe(nunjucksRender({
+      path: ["app/templates"]
+    })).pipe(
+      dest(siteOutput)
+    );
 }
 
-function sassDocTask () {
-  return src(input).pipe(sassdoc(sassdocOptions)).resume();
+function sassDocTask() {
+  return src(inputSass).pipe(sassdoc(sassdocOptions)).resume();
 }
 
+function copyStaticTask() {
+  return src(inputStatic).pipe(dest(siteOutput));
+}
+
+function copyCssTask() {
+  return src(inputCSS).pipe(dest(outputStyles));
+}
+
+function watchTask() {
+  watch(inputSass, sassTask);
+  watch([inputScripts], scriptTask).on('change', browserSync.reload);
+  watch([inputPages, inputTemplates], nunjucksTask).on('change', browserSync.reload);
+}
+
+function browserSyncTask() {
+  browserSync.init({
+    server: {
+      baseDir: siteOutput
+    }
+  });
+}
+
+exports.watch = watchTask;
 exports.clean = cleanTask;
 exports.sass = sassTask;
-exports.build = nunjucksTask;
-exports.default = series(cleanTask, nunjucksTask);
+exports.sassdoc = sassDocTask;
+exports.nunjucks = nunjucksTask;
+exports.scripts = scriptTask;
+exports.copystatic = series(copyStaticTask, copyCssTask);
+exports.build = series(nunjucksTask, sassTask, copyStaticTask, copyCssTask, scriptTask);
+exports.default = series(cleanTask, nunjucksTask, sassTask, sassDocTask, copyStaticTask, copyCssTask, scriptTask);
+exports.serve = parallel(browserSyncTask, watchTask);
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Quest for Game Dev: So You Want to Be a Hero
+
+## License
+
+MIT License -- see [LICENSE](LICENSE)
